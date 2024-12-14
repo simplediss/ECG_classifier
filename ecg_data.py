5367aeaf866e9eab2fe6b661e8a9d3a84bfb3722
linapetrovsky <118122434+linapetrovsky@users.noreply.github.com>
2024-12-14 17:12:43 +0200

    add code to handle ECG data

---
+++ ecg_data.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from functools import partial
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import dataset
+from plot import plot_ecg, save_as_png
+from sample import get_samples_paths, load_hea, load_mat, load_sample
+
+
+DATA_DIR = Path('./data/samples')
+SNOMED_DIR = Path('./data/snomed_mappings')
+RELEVANT_SNOMED_CSV = 'relevant_SNOMED_mappings.csv'
+SAMPLE_RATE = 500  # [Hz]
+
+dataset.download_kaggle_datasets(DATA_DIR)
+dataset.download_snomed_mappings(SNOMED_DIR) 
+dataset.create_relevant_snomed_mappings(SNOMED_DIR, RELEVANT_SNOMED_CSV)
+
+samples_paths = get_samples_paths(DATA_DIR, limit_sample_length=5000)
+snomed = pd.read_csv(SNOMED_DIR / RELEVANT_SNOMED_CSV, index_col='code')
+code_to_desc = partial(dataset.code_to_description, snomed=snomed)
+desc_to_code = partial(dataset.description_to_code, snomed=snomed)
+relevant_codes = snomed.index.to_list()
+
+
+flag = 0
+for path in samples_paths:
+    header = load_hea(path)
+    all_codes = header.codes
+    filtered_labels = header.filtered_codes(relevant_codes)
+    if not filtered_labels:
+        continue
+    data = load_mat(path)
+
+    if desc_to_code('myocardial ischemia') in filtered_labels and len(filtered_labels) == 1:
+        plot_ecg(data/1000, SAMPLE_RATE)
+        plt.show()
+        break
+        # save_as_png(f'./myocardial_ischemia/{path.name}')
+        # flag += 1
+        # if flag == 2:
+        #     break
+        
